c32fa408e61c283d48d8b04df91d12233d2aaf39
axlfrost <ale.gonzalezlevio@gmail.com>
2019-11-14 21:27:45 -0300

    Implementación de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
+// Rutas
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: LoginComponent },
 
-const routes: Routes = [];
+  { path: 'progress', component: ProgressComponent },
+  { path: 'graficas1', component: Graficas1Component },
+
+  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+  { path: '**', component: NopagefoundComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
